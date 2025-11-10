a894e35e89ad4c02176008b098dcb03fc345db56
Meixin Zhan <mzhan4027@gmail.com>
2025-11-10 00:29:08 +0000

    added means to update history list

--- numberGenerator.js
+++ numberGenerator.js
@@ -5,10 +5,24 @@
   - generate number once tapped again*/
 
 function generate(){
-  addEventListener("deviceorientationabsolute", function(){
-      document.getElementById('generate').innerHTML=getd20Roll();
-  })
+  number=getd20Roll();
+  document.getElementById('generate').innerHTML=number;
+}
 
+function addToHistory(){
+  //const list=document.createElement("li")
+  document.getElementById('history').innerHTML=
+            `<li class="list-group-item">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">List group item heading</h5>
+                    <small class="text-body-secondary" id="date">3 days ago</small>
+                </div>
+                <p class="mb-1" id="random">Some placeholder content in a paragraph.</p>
+                <small class="text-body-secondary">And some muted small print.</small>
+            </li>`;
+  
+  document.getElementById('random').innerHTML="lolxd";
+  document.getElementById('date').innerHTML=Date();
 }
 
 function getd20Roll() {
