4f310657381d3ea5e12fad2c610bde4fb2b7e16a
Meixin Zhan <mzhan4027@gmail.com>
2025-11-19 20:11:55 +0000

    list fix

--- numberGenerator.js
+++ numberGenerator.js
@@ -158,14 +158,13 @@ function historyListing(num){
   const liElement=document.createElement("li")
 
   liElement.className="list-group-item"
-  liElement.innerHTML=`<li class="list-group-item">
+  liElement.innerHTML=`
                 <div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">D${num.sidedDie}</h5>
                     <small class="text-body-secondary">${num.date}</small>
                 </div>
                 <p class="mb-1">${num.num}</p>
-                <small class="text-body-secondary">And some muted small print.</small>
-            </li>`;
+                <small class="text-body-secondary">And some muted small print.</small>`;
 
   return liElement;
 }
