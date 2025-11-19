af1d1baebb396f2cabca699155d6e58db63fd488
Meixin Zhan <mzhan4027@gmail.com>
2025-11-19 19:50:59 +0000

    history test

--- numberGenerator.js
+++ numberGenerator.js
@@ -146,20 +146,19 @@ function addToHistory(){
   }
 
   nums.forEach((num) => {
-      position=position+1
-      historyContainer.appendChild(historyListing(num, position));
+      historyContainer.appendChild(historyListing(num));
   });
 
 }
 
 /*Creates a list object to allow history*/
-function historyListing(num, position){
+function historyListing(num){
   const liElement=document.createElement("li")
 
   liElement.className="list-group-item"
   liElement.innerHTML=`<li class="list-group-item">
                 <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">`+position+`</h5>
+                    <h5 class="mb-1">E</h5>
                     <small class="text-body-secondary">${num.date}</small>
                 </div>
                 <p class="mb-1">${num.num}</p>
