bf12da57ade21ce7a9fba5cb7a795cf71464f770
Meixin Zhan <mzhan4027@gmail.com>
2025-11-24 02:32:52 +0000

    replaced placeholder images and added alt text

--- sw.js
+++ sw.js
@@ -9,6 +9,14 @@ self.addEventListener('install', event => {
                 '/style.css' ,
                 '/app.js' ,
                 '/manifest.json' ,
+                '/images/d4.gif' ,
+                '/images/d6.gif' ,
+                '/images/d8.gif' ,
+                '/images/d10.gif' ,
+                '/images/d12.gif' ,
+                '/images/d20.gif' ,
+                '/images/d20 roll.gif' ,
+                '/images/d100.gif' ,
                 '/images/icon.png'
             ]);
         })
